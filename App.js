23f0ca47e358640ba27569bf4c88c11664ca2221
Aleksander Truss <78085479+Sanyukkie@users.noreply.github.com>
2022-08-30 09:23:13 +0300

    Finished EX

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+
+// Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+
+  const Tab = createBottomNavigator();
+
+  const buttonPressed = () => { 
+    Alert.alert('You typed: ', text);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{width:200, borderColor: 'gray', borderWidth:1}}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <Button onPress={buttonPressed} title="Press me" />
       <StatusBar style="auto" />
     </View>
   );
